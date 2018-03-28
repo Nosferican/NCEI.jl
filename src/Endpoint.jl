f31be6769f358684b23f054a163f85d5cf212a4e
Nosferican <naoyabpr@gmail.com>
2018-03-28 16:34:51 -0400

    Fix

--- src/Endpoint.jl
+++ src/Endpoint.jl
@@ -8,11 +8,11 @@ function period(startdate::TimeType, enddate::TimeType, dataset::AbstractString)
 end
 
 for_assignment(::Type{Union{Missing, String}}, value::Missing) = missing
-for_assignment(::Type{Union{Missing, Float64}}, value::Missing) = missing
+for_assignment(::Type{Union{Missing, Float64}}, value::Number) = missing
 for_assignment(::Type{Union{Missing, Date}}, value::Missing) = missing
 for_assignment(::Type{Union{Missing, DateTime}}, value::Missing) = missing
 for_assignment(::Type{Union{Missing, String}}, value::AbstractString) = convert(String, value)
-for_assignment(::Type{Union{Missing, Float64}}, value::AbstractString) = convert(Float64, value)
+for_assignment(::Type{Union{Missing, Float64}}, value::Number) = convert(Float64, value)
 for_assignment(::Type{Union{Missing, Date}}, value::AbstractString) = Date(convert(String, value))
 for_assignment(::Type{Union{Missing, DateTime}}, value::AbstractString) = DateTime(convert(String, value))
 
